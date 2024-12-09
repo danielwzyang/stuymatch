77c6a762000cf3f44b2a141bb6b22b0d82b90a2c
Daniel Yang <danielwzyang@gmail.com>
2024-12-08 22:01:10 -0500

    added email regex check; added username character limit

--- app/register/client.tsx
+++ app/register/client.tsx
@@ -18,11 +18,14 @@ export default function ClientPage() {
 
         switch (step) {
             case 0:
-                if (email === "") setValid(false)
+                // RFC 5322 standard for email validation using regex
+                const valid = email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/) != null
+
+                if (email === "" || !valid) setValid(false)
                 else setValid(true)
                 break
             case 1:
-                if (!username) setValid(false)
+                if (!username || username.length < 3) setValid(false)
                 else setValid(true)
                 break
             case 2:
@@ -66,7 +69,10 @@ export default function ClientPage() {
                 return (
                     <>
                         <h1 className="text-center text-2xl my-3">Pick a username.</h1>
-                        <h1 className="text-[#808080] text-sm text-center mb-5">Can only contain letters and numbers.</h1>
+                        <div className="text-[#808080] text-sm text-center mb-5">
+                            <h1>Must be at least 3 characters</h1>
+                            <h1>Can only contain letters and numbers.</h1>
+                        </div>
                         <div className="flex justify-center">
                             <input
                                 className="w-[260px] px-3 py-2 border-[#d1d1d1] bg-[#e6e6e6] rounded-xl"
@@ -81,9 +87,7 @@ export default function ClientPage() {
                 return (
                     <>
                         <h1 className="text-center text-2xl my-3">Choose a password.</h1>
-                        <ul className="text-[#808080] text-sm text-center mb-5">
-                            <li>Must be at least 6 characters.</li>
-                        </ul>
+                        <h1 className="text-[#808080] text-sm text-center mb-5 mx-5">Must be at least 6 characters.</h1>
                         <div className="flex justify-center">
                             <input
                                 className="w-[260px] px-3 py-2 border-[#d1d1d1] bg-[#e6e6e6] rounded-xl"
